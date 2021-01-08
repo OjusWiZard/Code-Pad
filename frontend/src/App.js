b3835b84d1a5e64e4910af66ee14a1b100cbd73b
Shruti3004 <mail2shruti.ag@gmail.com>
2021-01-09 01:01:47 +0530

    Added routes in frontend folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,19 @@
+import React from "react";
 import "./App.css";
-import { useState } from "react";
-import Axios from "axios";
-import Editor from "./components/Editor/Editor";
+import { Route, Switch } from 'react-router-dom';
+import Form from "./components/editor/Form";
+import Home from "./components/home/Home";
+import Events from "./components/events/Events";
+
 function App() {
-  const [value, setValue] = useState("");
-  const [input, setInput] = useState("");
-  const langData = {
-    cpp: "text/x-c++src",
-    c: "text/x-csrc",
-    java: "text/x-java"
-  }
-  const [language, setLanguage] = useState("text/x-c++src");
-  const changeLanguage = (e) => {
-    setLanguage(e.target.value);
-  };
-
-
+  
   return (
     <div className="App">
-      <h2 align="center">NCS CODEPAD</h2>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          let data = {
-            code: value,
-            lang: Object.keys(langData).find(key => langData[key] === language),
-            input,
-          }
-          const config = {
-            headers: {
-              "Content-Type": "application/json"
-            }
-          }
-          console.log('ok', data);
-          try {
-
-            const res = await Axios.post("https://cors-anywhere.herokuapp.com/http://ide.shoa-apps.live/api/question/run", JSON.stringify(data), config)
-            console.log(res.data);
-          } catch (error) {
-            console.log(error);
-          }
-
-          console.log(language, JSON.stringify(value));
-        }}
-      >
-        <select onChange={(e) => changeLanguage(e)} value={language}>
-          <option value="text/x-c++src">C++</option>
-          <option value="text/x-csrc">C</option>
-          <option value="text/x-java">Java</option>
-        </select>
-        <Editor
-          language={language}
-          displayName="Code"
-          value={value}
-          input={input}
-          inputChange={setInput}
-          onChange={setValue}
-        ></Editor>
-        <button type="submit">Submit Code</button>
-      </form>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/editor" component={Form} />
+        <Route exact path="/events" component={Events} />
+      </Switch>      
     </div>
   );
 }
---
+++ frontend/src/components/events/Events.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Events() {
+    return (
+        <div>
+            Events
+        </div>
+    )
+}
+
+export default Events
