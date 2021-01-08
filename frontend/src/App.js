413022748faccff5490f019c372e8091434638ef
Shruti3004 <mail2shruti.ag@gmail.com>
2021-01-09 01:28:09 +0530

    Added default route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import Form from "./components/editor/Form";
 import Home from "./components/home/Home";
 import Events from "./components/events/Events";
+import Default from "./components/default/Default";
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/editor" component={Form} />
         <Route exact path="/events" component={Events} />
+        <Route component={Default} />
       </Switch>      
     </div>
   );
