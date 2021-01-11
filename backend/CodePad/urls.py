e121751412639eb267879f753396b4d8097c6f14
OjusWiZard <ojuswimail@gmail.com>
2021-01-11 18:08:24 +0530

    urls balanced

--- backend/CodePad/urls.py
+++ backend/CodePad/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('account.urls')),
-    path('', include('event.urls')),
+    path('funzone/', include('event.urls')),
     path('ide/', include('ide.urls'))
 ]
