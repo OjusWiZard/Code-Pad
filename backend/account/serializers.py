040a1c84906915690a000a9b7d1d176efcf4029f
OjusWiZard <ojuswimail@gmail.com>
2021-01-13 01:34:24 +0530

    fixed Submissions API

---
+++ backend/account/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import User
+
+
+class User_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
