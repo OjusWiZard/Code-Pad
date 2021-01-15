a97576b473e7f439e6f093e531781c2f3cbcf37b
Shruti3004 <mail2shruti.ag@gmail.com>
2021-01-15 20:11:48 +0530

    Updated nav-items

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,37 +1,35 @@
 import React from 'react'
 import './navbar.css'
 import Nav from "react-bootstrap/Nav";
-import { NavLink, Link } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
+import logo from '../../images/home/logo.png'
 
 function navbar() {
     return (
         <div>
             <nav className="navbar navbar-expand-lg">
               <Nav.Link className="navbar-brand" to="/">
+                {/* <img src={logo} alt="Code-pad" className="img-fluid my-2 mx-0 px-0"/> */}
                 Navbar
               </Nav.Link>
               <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon" />
                     </button>
                     <div className="collapse navbar-collapse" id="navbarText">
-                      <ul className="navbar-nav mx-auto mt-4">
-                        <li className="nav-item mx-2">
-                          <Nav.Link className="text-center nav-link font-grey font-16 font-vcr" href="/">
+                      <Nav className="navbar-nav mx-auto mt-4">                        
+                        {/* <NavLink exact to="/" activeClassName="active-nav-item">*/}
+                          <Nav.Link className="text-center nav-item mx-5 font-blue font-16 font-vcr" href="/">
                             HOME
-                          </Nav.Link>
-                        </li>
-                        <li className="nav-item mx-2">
-                          <Nav.Link className="text-center nav-link font-grey font-16 font-vcr" href="/events">
+                          </Nav.Link>                          
+                        {/* </NavLink>*/}
+                          <Nav.Link className="text-center nav-item mx-5 font-blue font-16 font-vcr" href="/events">
                             EVENTS
-                          </Nav.Link>
-                        </li>
-                        <li className="nav-item mx-2">
-                          <Nav.Link className="text-center nav-link font-grey font-16 font-vcr" href="/editor">
+                          </Nav.Link>                                                
+                          <Nav.Link className="text-center nav-item mx-5 font-blue font-16 font-vcr" href="/editor">
                             EDITOR
-                          </Nav.Link>
-                        </li>
-                      </ul>
-                      <Nav.Link className="text-center nav-link nav-item font-grey font-16 font-vcr" href="/login">
+                          </Nav.Link>                        
+                      </Nav>
+                      <Nav.Link className="text-center nav-item font-blue font-16 font-vcr" href="/login">
                          LOGIN
                       </Nav.Link>                      
                     </div>
