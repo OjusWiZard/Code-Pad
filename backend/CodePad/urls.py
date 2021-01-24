a7de1104d1f51f3f39bb76012a5d6e48a51690cc
OjusWiZard <ojuswimail@gmail.com>
2021-01-25 03:19:30 +0530

    removed deprecated ide app

--- backend/CodePad/urls.py
+++ backend/CodePad/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('account.urls')),
-    path('funzone/', include('event.urls')),
-    path('ide/', include('ide.urls'))
+    path('', include('event.urls'))
 ]
