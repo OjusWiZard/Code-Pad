ee9b9d4e016b0276176c85f0c6fd3198abfe1f01
SingingApple <anurag.as.srivastava@gmail.com>
2021-01-29 16:41:33 +0530

    Editor-css

--- frontend/src/components/editor/Form.jsx
+++ frontend/src/components/editor/Form.jsx
@@ -1,62 +1,101 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Axios from "axios";
+import "./Editor.css";
+import IDE from "../../images/editor/IDE.png";
 import Editor from "./Editor";
 
-function Form() {
-    const [value, setValue] = useState("");
-    const [input, setInput] = useState("");
-    const langData = {
-        cpp: "text/x-c++src",
-        c: "text/x-csrc",
-        java: "text/x-java"
-    }
-    const [language, setLanguage] = useState("text/x-c++src");
-    const changeLanguage = (e) => {
-        setLanguage(e.target.value);
-    };
-    return (
-        <React.Fragment>
-            <h2 align="center">NCS CODEPAD</h2>
-            <form
-                onSubmit={async (e) => {
-                e.preventDefault();
-                let data = {
-                    code: value,
-                    lang: Object.keys(langData).find(key => langData[key] === language),
-                    input,
-                }
-                const config = {
-                    headers: {
-                    "Content-Type": "application/json"
-                    }
-                }
-                console.log('ok', data);
-                try {
-                    const res = await Axios.post("https://cors-anywhere.herokuapp.com/http://ide.shoa-apps.live/api/question/run", JSON.stringify(data), config)
-                    console.log(res.data);
-                } catch (error) {
-                    console.log(error);
-                }
-                console.log(language, JSON.stringify(value));
-                }}
-            >
-                <select onChange={(e) => changeLanguage(e)} value={language}>
-                    <option value="text/x-c++src">C++</option>
-                    <option value="text/x-csrc">C</option>
-                    <option value="text/x-java">Java</option>
-                </select>
-                <Editor
-                language={language}
-                displayName="Code"
-                value={value}
-                input={input}
-                inputChange={setInput}
-                onChange={setValue}
-                ></Editor>
-                <button type="submit">Submit Code</button>
-            </form>
-        </React.Fragment>
-    )
-}
+const Form = () => {
+  const [value, setValue] = useState("");
+  const [input, setInput] = useState("");
+  const langData = {
+    cpp: "text/x-c++src",
+    c: "text/x-csrc",
+    java: "text/x-java",
+  };
 
-export default Form
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let data = {
+      code: value,
+      lang: Object.keys(langData).find((key) => langData[key] === language),
+      input,
+    };
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    console.log("ok", data);
+    try {
+      const res = await Axios.post(
+        "https://cors-anywhere.herokuapp.com/http://ide.shoa-apps.live/api/question/run",
+        JSON.stringify(data),
+        config
+      );
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(language, JSON.stringify(value));
+  };
+  const [language, setLanguage] = useState("text/x-c++src");
+  const changeLanguage = (e) => {
+    setLanguage(e.target.value);
+  };
+  return (
+    <React.Fragment>
+      <div className="main-background">
+        <div className="container pt-5">
+          <div className="row">
+            <div className="col-lg-8 col-md-8 col-sm-10 col-10 mx-auto my-5 content-background px-5">
+              <div className="d-flex justify-content-center pt-5">
+                <img src={IDE} alt="IDE" />
+              </div>
+              <p className="font-robot font-lightGrey text-justify mt-5 pt-3 font-14">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. amet
+                qui! Sed, distinctio maiores consequatur velit, voluptatibus
+                deleniti voluptate suscipit corrupti odio amet inventore sunt,
+                esse molestiae et iusto tenetur laboriosam ipsam ab nemo harum
+                impedit.
+              </p>
+              <React.Fragment>
+                <h2 align="center">NCS CODEPAD</h2>
+                <form onSubmit={(e) => handleSubmit(e)}>
+                  <div className="d-flex font-vcr justify-content-between align-items-center py-3">
+                    <select
+                      onChange={(e) => changeLanguage(e)}
+                      value={language}
+                    >
+                      <option value="text/x-c++src">C++</option>
+                      <option value="text/x-csrc">C</option>
+                      <option value="text/x-java">Java</option>
+                    </select>
+                    <span>X</span>
+                  </div>
+                  <Editor
+                    language={language}
+                    displayName="Code"
+                    value={value}
+                    input={input}
+                    onChange={setValue}
+                  ></Editor>
+                  <div className="d-flex align-items-start justify-content-between mt-3">
+                    <input
+                      type="text"
+                      value={input}
+                      onChange={(e) => setInput(e.target.value)}
+                      id="custom"
+                    />
+                    <button type="submit">Submit Code</button>
+                  </div>
+                </form>
+              </React.Fragment>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Form;
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,40 +1,88 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import "./navbar.css";
 import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
-import logo from '../../images/home/logo.png'
+import logo from "../../images/home/logo.png";
 
 function navbar() {
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg">
-              <Nav.Link className="navbar-brand" href="/">
-                <img src={logo} alt="Code-pad" className="img-fluid mx-0 px-0" width="120px"/>                
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg">
+        <Nav.Link className="navbar-brand" href="/">
+          <img
+            src={logo}
+            alt="Code-pad"
+            className="img-fluid mx-0 px-0"
+            width="120px"
+          />
+        </Nav.Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <Nav className="navbar-nav mx-auto mt-5">
+            <NavLink
+              exact
+              to="/"
+              activeClassName="active-nav-item"
+              className="mr-5 "
+            >
+              <Nav.Link
+                className="text-center font-16 font-vcr font-blue nav-item"
+                href="/"
+              >
+                HOME
               </Nav.Link>
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon" />
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarText">
-                      <Nav className="navbar-nav mx-auto mt-5">                        
-                        <NavLink exact to="/" activeClassName="active-nav-item" className="mr-5 ">
-                          <Nav.Link className="text-center font-16 font-vcr font-blue nav-item" href="/">
-                            HOME
-                          </Nav.Link>                          
-                        </NavLink>
-                          <Nav.Link className="text-center nav-item mx-5 font-blue font-16 font-vcr" href="/events">
-                            EVENTS
-                          </Nav.Link>                                                
-                          <Nav.Link className="text-center nav-item mx-5 font-blue font-16 font-vcr" href="/editor">
-                            EDITOR
-                          </Nav.Link>                        
-                      </Nav>
-                      <Nav.Link className="text-center nav-item font-blue font-16 font-vcr mt-n2" href="/login">
-                         LOGIN
-                      </Nav.Link>                      
-                    </div>
-                  </nav>
+            </NavLink>
+            <NavLink
+              to="/events"
+              activeClassName="active-nav-item"
+              className="mr-5"
+            >
+              <Nav.Link
+                className="text-center nav-item mx-5 font-blue font-16 font-vcr"
+                href="/events"
+              >
+                EVENTS
+              </Nav.Link>
+            </NavLink>
+            <NavLink
+              to="/editor"
+              activeClassName="active-nav-item"
+              className="mr-5"
+            >
+              <Nav.Link
+                className="text-center nav-item mx-5 font-blue font-16 font-vcr"
+                href="/editor"
+              >
+                EDITOR
+              </Nav.Link>
+            </NavLink>
+          </Nav>
+          <NavLink
+            to="/login"
+            activeClassName="active-nav-item"
+            className="mr-5"
+          >
+            <Nav.Link
+              className="text-center nav-item font-blue font-16 font-vcr mt-n2"
+              href="/login"
+            >
+              LOGIN
+            </Nav.Link>
+          </NavLink>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
 
-export default navbar
+export default navbar;
