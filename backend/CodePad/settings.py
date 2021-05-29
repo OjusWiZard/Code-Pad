1e667c4f6a6a8fa9989f6ca4dbda5d4cd0c6e00b
OjusWiZard <ojuswimail@gmail.com>
2021-05-29 23:30:35 +0530

    extended refresh token lifetime

--- backend/CodePad/settings.py
+++ backend/CodePad/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from datetime import timedelta
+from datetime import time, timedelta
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -64,6 +64,7 @@ DJOSER = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(weeks=4)
 }
 
 MIDDLEWARE = [
