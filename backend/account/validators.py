40929ba09d1cdff9cf06343af249be1e4ab7335b
OjusWiZard <ojuswimail@gmail.com>
2021-05-29 20:35:59 +0530

    shortend error line in validator

--- backend/account/validators.py
+++ backend/account/validators.py
@@ -3,7 +3,7 @@ import re
 
 def validate_admission_number(number):
     match = re.match(pattern='([0-2][0-9])(CS|IT|EE|ECE|EEE|CE|IC|ME|MT)([0-9]{3})', string=number)
-    error_message = 'Enter a valid value. Make sure that all the alphabets are capital. Or, match the value with the regex ([0-2][0-9])(CS|IT|EE|ECE|EEE|CE|IC|ME|MT)([0-9]{3})'
+    error_message = 'Enter a valid value. Make sure that all the alphabets are capital.'
     if match is None:
         raise ValidationError(error_message)
     elif match.span()[1] < len(number):
