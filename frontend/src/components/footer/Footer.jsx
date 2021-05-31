7d8090595fc8b1aefe5b47a30145ed91344b690b
Shruti3004 <mail2shruti.ag@gmail.com>
2021-05-31 15:36:21 +0530

    added the pixelated inputs and background of root

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -4,18 +4,20 @@ import heart from "../../images/footer/heart.png";
 
 function Footer() {
   return (
-    <div className="text-center bg-black font-lightGrey font-vcr pt-4">
-      <p className="px-3">
-        MADE WITH <img src={heart} alt="heart" className="img-fluid" /> BY
-        NIBBLE COMPUTER SOCIETY
-      </p>
+    <>
+      <div className="container font-lightGrey font-vcr pt-4">
+        <p className="px-3 text-center">
+          MADE WITH <img src={heart} alt="heart" className="img-fluid" /> BY
+          NIBBLE COMPUTER SOCIETY
+        </p>
+      </div>
       <img
         src={borderBottom}
         alt="illustration"
         className="img-fluid"
         style={{ width: "100%" }}
       />
-    </div>
+    </>
   );
 }
 
