a6b86dc861d8f941c9239820a6cf4640f827b1a9
OjusWiZard <ojuswimail@gmail.com>
2021-06-07 16:18:18 +0530

    added contact_no field and removed last_name field

--- backend/account/validators.py
+++ backend/account/validators.py
@@ -8,3 +8,10 @@ def validate_admission_number(number):
         raise ValidationError(error_message)
     elif match.span()[1] < len(number):
         raise ValidationError(error_message)
+
+def validate_contact_number(number):
+    match = re.match(pattern='^[6-9]\d{9}$', string=number)
+    error_message = "Enter a valid Contact number."
+    if match is None:
+        raise ValidationError(error_message)
+        
