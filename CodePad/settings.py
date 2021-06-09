a4ab75d240ca72453e3ff0dddcad167fd23a3684
OjusWiZard <ojuswimail@gmail.com>
2021-06-10 02:16:04 +0530

    installed whitenoise

--- CodePad/settings.py
+++ CodePad/settings.py
@@ -75,6 +75,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'CodePad.urls'
