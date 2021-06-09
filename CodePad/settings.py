43ea3e2a08f56d198ff4dfa4ebeb94f33b7cc043
OjusWiZard <ojuswimail@gmail.com>
2021-06-10 03:08:01 +0530

    connected to heroku postgres

--- CodePad/settings.py
+++ CodePad/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from datetime import time, timedelta
+from datetime import timedelta
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,6 +117,9 @@ DATABASES = {
         'PORT': os.environ['DB_PORT']
     }
 }
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
