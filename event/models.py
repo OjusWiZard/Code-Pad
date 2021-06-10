fa9390287f664a9df4b8f7f17d27ff5607128886
OjusWiZard <ojuswimail@gmail.com>
2021-06-10 18:38:55 +0530

    added event icon

--- event/models.py
+++ event/models.py
@@ -9,6 +9,7 @@ class Event(models.Model):
     title       = models.CharField(max_length=32)
     description = models.TextField(max_length=1024)
     is_contest  = models.BooleanField(default=True)
+    icon        = models.ImageField(upload_to='event_icons/', blank=True)
     datetime    = models.DateTimeField(auto_now=False)
     duration    = models.DurationField(default=timedelta(hours=2))
     
