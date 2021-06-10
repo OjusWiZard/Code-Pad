3000e62462d3e080d8abdd1f8d503f5d2b188fbc
OjusWiZard <ojuswimail@gmail.com>
2021-06-10 19:08:44 +0530

    allowed all host

--- CodePad/settings.py
+++ CodePad/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost',os.environ['SERVER_HOST']]
+# ALLOWED_HOSTS = ['localhost',os.environ['SERVER_HOST']]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
