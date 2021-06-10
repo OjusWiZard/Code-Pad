6c092bf8a1e1514053b864f57a220aecfe87a3dc
Shruti3004 <mail2shruti.ag@gmail.com>
2021-06-11 00:12:23 +0530

    fixed the home pge

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -5,8 +5,8 @@ import heart from "../../images/footer/heart.svg";
 function Footer() {
   return (
     <>
-      <div className="text-center font-lightGrey font-vcr pt-4">
-        <p className="px-3 text-center">
+      <div className="font-lightGrey font-vcr pt-4">
+        <p className="px-3 text-center mb-0">
           MADE WITH <img src={heart} alt="heart" className="img-fluid" /> BY
           NIBBLE COMPUTER SOCIETY
         </p>
