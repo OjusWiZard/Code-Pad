c287ccd59ee47904764a63fa0c17e69c59a12d1b
OjusWiZard <ojuswimail@gmail.com>
2021-06-12 03:30:26 +0530

    added variable scores

--- event/models.py
+++ event/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 from django.utils.timezone import timedelta
 from account.models import User
 from .validators import validate_text_file
@@ -8,7 +9,7 @@ class Event(models.Model):
     slug        = models.SlugField(max_length=8, unique=True)
     title       = models.CharField(max_length=32)
     description = models.TextField(max_length=1024)
-    is_contest  = models.BooleanField(default=True)
+    is_contest  = models.BooleanField(default=True, help_text='True if you want Scores and Leaderboard.')
     icon        = models.ImageField(upload_to='event_icons/')
     datetime    = models.DateTimeField(auto_now=False)
     duration    = models.DurationField(default=timedelta(hours=2))
@@ -49,9 +50,22 @@ class Problem(models.Model):
     example_explanation = models.TextField(max_length=1024, blank=True, null=True)
     solution_input      = models.FileField(upload_to=get_input_testcase_path, validators=[validate_text_file])
     solution_output     = models.FileField(upload_to=get_output_testcase_path, validators=[validate_text_file])
+    points              = models.PositiveSmallIntegerField(default=150, help_text='Maximum score.')
+    penalty             = models.PositiveSmallIntegerField(default=20, help_text='Points Deduction on Incorrect Attempt.')
+    point_loss          = models.PositiveSmallIntegerField(default=2, help_text='Point loss per minute.')
 
     event               = models.ForeignKey(Event, on_delete=models.CASCADE)
     submission_from     = models.ManyToManyField(User, through='Submission')
+
+    def clean(self, *args, **kwargs):
+        minutes =  self.event.duration.total_seconds() // 60
+        if minutes * self.point_loss > self.points * 70 / 100:
+            raise ValidationError('Point Loss per minute is too high, points should not get less than 30% of the initial value.\nRefer https://codeforces.com/blog/entry/456')
+        super().clean(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
     
     def __str__(self):
         return self.title
