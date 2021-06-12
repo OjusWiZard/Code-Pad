d26e1f96f24dae3a2f6131966d12042a8535c553
Shruti3004 <mail2shruti.ag@gmail.com>
2021-06-12 20:04:29 +0530

    refactored the code structure

--- client/src/components/events/Events.jsx
+++ client/src/components/events/Events.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import frame from "../../images/events/Frame.svg";
 import logo from "../../images/events/event_logo.svg";
 import line from "../../images/home/line.png";
 import { Link } from "react-router-dom";
 import "./events.css";
 import { getAllEvents } from "../../api/index";
 import Spinner from "../utils/Spinner";
+import Event from "../utils/Event";
 
 function Events() {
   const [allEvents, setallEvents] = useState([]);
@@ -36,108 +36,48 @@ function Events() {
                 <div className="d-flex justify-content-center pt-5">
                   <img src={logo} alt="" className="img-fluid" />
                 </div>
-                <h4
-                  className="font-vcr font-blue mt-5 pt-4 text-center font-weight-bold"
-                  id="ongoing"
-                >
-                  &lt;&lt;&nbsp;&nbsp;ONGOING EVENTS&nbsp;&nbsp;&gt;&gt;
-                </h4>
-                <div className="row d-flex justify-content-center mt-3">
-                  {onGoingEvents?.map((event, index) => (
-                    <Link
-                      key={event.url}
-                      to={`/events/${event.slug}`}
-                      className="button-hover link col-lg-4 col-md-4 col-sm-12 col-12 mt-3"
-                    >
-                      {console.log(event)}
-                      <div className="text-center">
-                        <img
-                          src={event.icon}
-                          alt=""
-                          className="img-fluid frame"
-                        />
-                        <div className="text-center pt-3">
-                          <span className="font-vcr font-blue">
-                            {event.title}
-                          </span>
-                          <p className="date pt-1 font-robot">
-                            {event.datetime}
-                          </p>
-                        </div>
-                      </div>
-                    </Link>
-                  ))}
-                </div>
+                {onGoingEvents && (
+                  <div>
+                    <h4 className="font-vcr font-blue mt-5 pt-4 text-center font-weight-bold">
+                      &lt;&lt;&nbsp;ONGOING EVENTS&nbsp;&gt;&gt;
+                    </h4>
+                    <div className="row d-flex justify-content-center mt-3">
+                      {onGoingEvents?.map((event) => (
+                        <Event event={event} />
+                      ))}
+                    </div>
+                  </div>
+                )}
                 <div className="d-flex justify-content-center pt-5">
                   <img src={line} alt="" className="img-fluid" />
                 </div>
-                <h4
-                  className="font-vcr font-blue mt-5 pt-4 text-center font-weight-bold"
-                  id="ongoing"
-                >
-                  &lt;&lt;&nbsp;&nbsp;UPCOMING EVENTS&nbsp;&nbsp;&gt;&gt;
-                </h4>
-                <div className="row d-flex justify-content-center mt-3">
-                  {upcomingEvents?.map((event, index) => (
-                    <Link
-                      key={event.url}
-                      to={`/events/${event.slug}`}
-                      className="button-hover link col-lg-4 col-md-4 col-sm-12 col-12 mt-3"
-                    >
-                      {console.log(event)}
-                      <div className="text-center">
-                        <img
-                          src={event.icon}
-                          alt=""
-                          className="img-fluid frame"
-                        />
-                        <div className="text-center pt-3">
-                          <span className="font-vcr font-blue">
-                            {event.title}
-                          </span>
-                          <p className="date pt-1 pb-4 font-robot">
-                            {event.datetime}
-                          </p>
-                        </div>
-                      </div>
-                    </Link>
-                  ))}
-                </div>
+                {upcomingEvents && (
+                  <div>
+                    <h4 className="font-vcr font-blue mt-5 pt-4 text-center font-weight-bold">
+                      &lt;&lt;&nbsp;UPCOMING EVENTS&nbsp;&gt;&gt;
+                    </h4>
+                    <div className="row d-flex justify-content-center mt-3">
+                      {upcomingEvents?.map((event) => (
+                        <Event event={event} />
+                      ))}
+                    </div>
+                  </div>
+                )}
                 <div className="d-flex justify-content-center pt-4">
                   <img src={line} alt="" className="img-fluid" />
                 </div>
-                <h4
-                  className="font-vcr font-blue mt-5 pt-3 text-center font-weight-bold"
-                  id="ongoing"
-                >
-                  &lt;&lt;&nbsp;&nbsp;PAST EVENTS&nbsp;&nbsp;&gt;&gt;
-                </h4>
-                <div className="row d-flex justify-content-center mt-3">
-                  {pastEvents?.map((event, index) => (
-                    <Link
-                      key={event.url}
-                      to={`/events/${event.slug}`}
-                      className="button-hover link col-lg-4 col-md-4 col-sm-12 col-12 mt-3"
-                    >
-                      {console.log(event)}
-                      <div className="text-center">
-                        <img
-                          src={event.icon}
-                          alt=""
-                          className="img-fluid frame"
-                        />
-                        <div className="text-center pt-3">
-                          <span className="font-vcr font-blue">
-                            {event.title}
-                          </span>
-                          <p className="date pt-1 pb-4 font-robot">
-                            {event.datetime}
-                          </p>
-                        </div>
-                      </div>
-                    </Link>
-                  ))}
-                </div>
+                {pastEvents && (
+                  <div>
+                    <h4 className="font-vcr font-blue mt-5 pt-4 text-center font-weight-bold">
+                      &lt;&lt;&nbsp;PAST EVENTS&nbsp;&gt;&gt;
+                    </h4>
+                    <div className="row d-flex justify-content-center mt-3">
+                      {pastEvents?.map((event) => (
+                        <Event event={event} />
+                      ))}
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           </div>
---
+++ client/src/components/utils/Event.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Event({ event }) {
+  return (
+    <div className="col-lg-4 col-md-4 col-sm-12 col-12 ">
+      <Link
+        key={event.url}
+        to={`/events/${event.slug}`}
+        className="button-hover link mt-3"
+      >
+        {console.log(event)}
+        <div className="text-center">
+          <img src={event.icon} alt="" className="img-fluid frame p-3" />
+          <div className="text-center pt-3">
+            <span className="font-vcr font-blue">{event.title}</span>
+            <p className="date pt-1 pb-4 font-robot">{event.datetime}</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default Event;
