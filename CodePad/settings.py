3e90b67ba181abe9a7ec10c7c4700dd8ac526057
OjusWiZard <ojuswimail@gmail.com>
2021-06-18 18:31:27 +0530

    using dotenv now

--- CodePad/settings.py
+++ CodePad/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 import dj_database_url
-import cloudinary
-import cloudinary_storage
 from pathlib import Path
 from datetime import timedelta
-import dj_database_url
+from dotenv import load_dotenv
+
+# Load environment variable from .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
