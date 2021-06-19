dbdfc098117875f6ba3521bd60dfb91b1b0ddb8a
SingingApple <anurag.as.srivastava@gmail.com>
2021-06-20 00:49:43 +0530

    Monaco

--- client/src/components/editor/Editor.jsx
+++ client/src/components/editor/Editor.jsx
@@ -1,34 +1,76 @@
 import { Controlled as CodeMirror } from "react-codemirror2";
 import "codemirror/lib/codemirror.css";
-import "codemirror/theme/material.css";
-import "codemirror/addon/edit/closebrackets";
-import "codemirror/addon/edit/matchbrackets";
-import "codemirror/addon/edit/closetag";
-import "codemirror/mode/clike/clike";
+import Editor1 from "@monaco-editor/react";
 import React from "react";
 
 const Editor = ({ language, value, onChange, inputChange, input }) => {
-  const handleChange = (editor, data, value) => {
-    onChange(value);
-  };
-
+  // const handleChange = (editor, data, value) => {
+  //   onChange(value);
+  // };
   return (
     <React.Fragment>
-      <CodeMirror
-        onBeforeChange={handleChange}
-        value={value}
+      <hr />
+      <Editor1
         className="code-mirror-wrapper"
+        defaultLanguage="c"
+        theme="vs-dark"
+        height="50vh"
         options={{
-          lineWrapping: true,
-          lint: true,
-          mode: "text/x-csrc",
-          theme: "material",
-          lineNumbers: true,
-          matchBrackets: true,
-          autoCloseBrackets: true,
-          autoCloseTags: true,
+          acceptSuggestionOnCommitCharacter: "true",
+          acceptSuggestionOnEnter: "on",
+          accessibilitySupport: "auto",
+          autoIndent: "true",
+          automaticLayout: "true",
+          codeLens: "true",
+          colorDecorators: "true",
+          contextmenu: "true",
+          cursorBlinking: "blink",
+          cursorSmoothCaretAnimation: "true",
+          cursorStyle: "line",
+          disableLayerHinting: "true",
+          disableMonospaceOptimizations: "true",
+          dragAndDrop: "true",
+          fixedOverflowWidgets: "true",
+          folding: "true",
+          foldingStrategy: "auto",
+          fontLigatures: "true",
+          formatOnPaste: "true",
+          formatOnType: "true",
+          hideCursorInOverviewRuler: "true",
+          highlightActiveIndentGuide: "true",
+          links: "false",
+          mouseWheelZoom: "false",
+          multiCursorMergeOverlapping: "true",
+          multiCursorModifier: "alt",
+          overviewRulerBorder: "true",
+          overviewRulerLanes: 2,
+          quickSuggestions: "true",
+          quickSuggestionsDelay: 100,
+          renderControlCharacters: true,
+          renderFinalNewline: "true",
+          renderIndentGuides: "true",
+          renderLineHighlight: "all",
+          renderWhitespace: "none",
+          revealHorizontalRightPadding: 30,
+          roundedSelection: "true",
+          scrollBeyondLastColumn: 5,
+          scrollBeyondLastLine: "true",
+          selectOnLineNumbers: "true",
+          selectionClipboard: "true",
+          selectionHighlight: "true",
+          showFoldingControls: "mouseover",
+          smoothScrolling: true,
+          suggestOnTriggerCharacters: "true",
+          wordBasedSuggestions: "true",
+          wordWrap: "off",
+          wordWrapBreakAfterCharacters: "\t})]?|&,;",
+          wordWrapBreakBeforeCharacters: "{([+",
+          wordWrapBreakObtrusiveCharacters: ".",
+          wordWrapColumn: 80,
+          wordWrapMinified: "true",
+          wrappingIndent: "none",
         }}
-      ></CodeMirror>
+      />
     </React.Fragment>
   );
 };
