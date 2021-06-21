beb0c18d01066df24abaada7063ffb71bc3b6911
OjusWiZard <ojuswimail@gmail.com>
2021-06-21 20:32:30 +0530

    added Leaderboard API

--- event/urls.py
+++ event/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import Event_Viewset, Problem_Viewset, Submission_Viewset
+from .views import Event_Viewset, Problem_Viewset, Submission_Viewset, Leaderboard_Viewset
 
 routers = routers.DefaultRouter()
 routers.register(r'events', Event_Viewset)
 routers.register(r'problems', Problem_Viewset)
 routers.register(r'submissions', Submission_Viewset)
+routers.register(r'leaderboard', Leaderboard_Viewset)
 
 urlpatterns = [
     path('', include(routers.urls)),
