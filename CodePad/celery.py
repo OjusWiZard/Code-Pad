2bf314992e6773f8d4a2ba4cf38d6ea1cc63ec5e
OjusWiZard <ojuswimail@gmail.com>
2021-07-08 22:56:01 +0530

    resetted rabbitmq

--- CodePad/celery.py
+++ CodePad/celery.py
@@ -4,11 +4,13 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CodePad.settings")
 
-envvars = os.environ.keys()
-if "RABBITMQ_URL" in envvars:
-    broker_url = os.environ["RABBITMQ_URL"]
-else:
-    broker_url = "amqp://guest:guest@rabbit:5672"
+broker_vhost = os.environ["RABBITMQ_DEFAULT_VHOST"]
+broker_user = os.environ["RABBITMQ_DEFAULT_USER"]
+broker_pass = os.environ["RABBITMQ_DEFAULT_PASS"]
+broker_host = os.environ["RABBITMQ_DEFAULT_HOST"]
+broker_url = (
+    "amqp://" + broker_user + ":" + broker_pass + "@" + broker_host + "/" + broker_vhost
+)
 
 app = Celery("CodePad", broker=broker_url)
 app.config_from_object("django.conf:settings", namespace="CELERY")
