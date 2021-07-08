3a93699c6a4d591642cc7a1196024f3688e7dee0
Shruti3004 <mail2shruti.ag@gmail.com>
2021-07-08 17:05:57 +0530

    udated the clickable functionality
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- client/src/components/Guards/PrivateLayout.js
+++ client/src/components/Guards/PrivateLayout.js
@@ -2,13 +2,13 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import { Route, Redirect } from "react-router-dom";
 
-let user = localStorage.getItem("user") || null;
+let token = localStorage.getItem("refreshToken") || null;
 
 const PrivateLayout = ({ component: Component, ...rest }) => (
   <Route
     {...rest}
     render={(props) =>
-      user ? (
+      token ? (
         <>
           <Component {...props} />
         </>
