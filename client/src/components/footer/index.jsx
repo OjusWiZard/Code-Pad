b1947b9ef3efd51505908ae21475451687660609
OjusWiZard <ojuswimail@gmail.com>
2021-12-15 01:33:58 +0530

    added a better footer
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- client/src/components/footer/index.jsx
+++ client/src/components/footer/index.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 
 // Image imports
-import borderBottom from "../../images/footer/borderBottom.svg";
-import heart from "../../images/footer/heart.svg";
+import footer from "../../images/footer/footer.svg";
 
 function Footer() {
   return (
     <footer>
-      <div className="font-lightGrey font-vcr mt-4">
-        <p className="px-3 text-center mb-0">
-          MADE WITH <img src={heart} alt="heart" className="img-fluid" width="15px" height="15px"/> BY
-          NIBBLE COMPUTER SOCIETY
-        </p>
-      </div>
       <img
-        src={borderBottom}
-        alt="illustration"
+        src={footer}
+        alt="Footer"
         className="img-fluid"
         style={{ width: "100%" }}
       />
